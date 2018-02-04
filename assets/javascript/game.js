48b7ba70df3105ba10d610943eede7c4289bb2a5
Melinda Hardin <melindawad@gmail.com>
2018-02-03 22:07:49 -0600

    Feb 3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,13 +6,9 @@ var lettersRight=[];
 var lettersGuessed;
 var lettersWrong=[];
 
+var alphabet = ["a", "b", "c", "d", "e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-
-var randomLetters= ["b", "c", "h", "l", "m", "s", "w", "o"];
-
-for (i=0; i<randomLetters.length; i++){
-    console.log(randomLetters[i]);
-}
+var randomLetters = alphabet[Math.floor(Math.random() * alphabet.length)];
 
 document.onkeyup = function (event) {
     var userGuess = event.key.toLowerCase();
@@ -22,21 +18,23 @@ document.onkeyup = function (event) {
         console.log(userGuess);
         wins++;
         lettersRight.push(userGuess);
+        alert ("Good guess!");  
     }
     
     else{
         guessesLeft--;
         lettersWrong.push(userGuess);
-        
     }
 
     if(guessesLeft === 0){
         losses++;
         var nextGame = confirm("Want to play agian?");
         if (nextGame === true){
-            
-
+            guessesLeft = 10;
+            lettersRight=[];
+            lettersWrong=[];
         }
+
         else{
             alert ("See you next time!");
             
